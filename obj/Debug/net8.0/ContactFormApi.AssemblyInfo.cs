c8b92dc05aeec88412dce32d27f5b2f4f887c854
Anindo Roy <anindoroy112@gmail.com>
2025-11-08 13:19:03 +0600

    Add Nginx config for admin.ashaa.xyz proxy
    Introduces setup-nginx-domain-xyz.conf to proxy requests for admin.ashaa.xyz to localhost:5050. Updates assembly info files with minor version metadata changes.

--- obj/Debug/net8.0/ContactFormApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContactFormApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactFormApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+553bfc3308259d6712900d29cbda348d094f647d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactFormApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactFormApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
