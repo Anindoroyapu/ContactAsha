cc9da6d3916a215189824f9eec4f90e8b1fb1e80
Anindo Roy <anindoroy112@gmail.com>
2025-12-29 23:24:39 +0600

    Add AshaContact model and controller
    Introduces the AshaContact entity, its API controller for CRUD operations, and registers the DbSet in AppDbContext. This enables management of contact form submissions via RESTful endpoints.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,6 +5,7 @@ using AddProductApi.Models;
 using ExpenditureFormApi.Models;
 using CollectionFormApi.Models;
 using CheckoutFormApi.Models;
+using AshaContactFormApi.Models;
 using AshaCollectionFormApi.Models;
 using AshaExpenditureFormApi.Models;
 
@@ -21,12 +22,13 @@ namespace AshaApi.Data
         public DbSet<Booking> Bookings { get; set; }  
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<AddProduct> AddProducts { get; set; }
-
         public DbSet<Expenditure> Expenditures { get; set; }
         public DbSet<AshaExpenditure> AshaExpenditures { get; set; }
         public DbSet<Collection> Collections { get; set; }
+         public DbSet<Checkout> Checkouts { get; set; }
+         public DbSet<AshaContact> AshaContacts { get; set; }
         public DbSet<AshaCollection> AshaCollections { get; set; }
-        public DbSet<Checkout> Checkouts { get; set; }
+        public DbSet<Checkout> Checkout { get; set; }
 
 
     }
