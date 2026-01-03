eacb4cd3abd4b38543ae2501652da264345c7eb4
Anindo Roy <142706157+Anindoroyapu@users.noreply.github.com>
2026-01-03 16:54:58 +0600

    update

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,11 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ContactFormApi.Models;
 using BookingFormApi.Models;
 using AddProductApi.Models;
 using ExpenditureFormApi.Models;
 using CollectionFormApi.Models;
 using CheckoutFormApi.Models;
-using AshaContactFormApi.Models;
 using AshaCollectionFormApi.Models;
 using AshaExpenditureFormApi.Models;
 using MooniAddProductApi.Models;
@@ -23,16 +22,13 @@ namespace AshaApi.Data
         public DbSet<Booking> Bookings { get; set; }  
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<AddProduct> AddProducts { get; set; }
+
         public DbSet<Expenditure> Expenditures { get; set; }
         public DbSet<AshaExpenditure> AshaExpenditures { get; set; }
         public DbSet<Collection> Collections { get; set; }
-         public DbSet<Checkout> Checkouts { get; set; }
-         public DbSet<AshaContact> AshaContacts { get; set; }
         public DbSet<AshaCollection> AshaCollections { get; set; }
         public DbSet<Checkout> Checkouts { get; set; }
         public DbSet<MooniAddProduct> MooniAddProducts { get; set; }
-        public DbSet<Checkout> Checkout { get; set; }
-
 
 
     }
